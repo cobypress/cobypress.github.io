ccaa44bd41b0a3b793a2a474031fe1c2a0279e6c
cobypress <hi@coby.press>
2023-05-03 21:52:10 +0100

    major update

--- quiz.js
+++ quiz.js
@@ -1,162 +1,7 @@
-const questions = [
-    {
-        question: "When trying to learn something new, you prefer:",
-        options: [
-            "Visual aids, such as graphs and diagrams",
-            "Listening to someone explain the topic",
-            "Reading about the topic",
-            "Hands-on practice and experimentation",
-        ],
-        answer: { A: "Visual", B: "Auditory", C: "ReadWrite", D: "Kinaesthetic" },
-    },
-    {
-        question: "When studying, you find it most helpful to:",
-        options: [
-            "Use flashcards with images",
-            "Listen to a recording of the material",
-            "Rewrite your notes",
-            "Create physical models or role-play",
-        ],
-        answer: { A: "Visual", B: "Auditory", C: "ReadWrite", D: "Kinaesthetic" },
-    },
-    {
-        question: "When learning a new dance step, you would prefer:",
-        options: [
-            "Watching a video demonstration",
-            "Listening to verbal instructions",
-            "Reading step-by-step instructions",
-            "Practicing the steps yourself",
-        ],
-        answer: { A: "Visual", B: "Auditory", C: "ReadWrite", D: "Kinaesthetic" },
-    },
-    {
-        question: "When trying to remember directions, you:",
-        options: [
-            "Visualize a map in your mind",
-            "Recall the spoken instructions",
-            "Write down the directions",
-            "Mentally retrace your steps",
-        ],
-        answer: { A: "Visual", B: "Auditory", C: "ReadWrite", D: "Kinaesthetic" },
-    },
-    {
-        question: "When learning a new language, you find it most helpful to:",
-        options: [
-            "Look at pictures with captions",
-            "Listen to native speakers",
-            "Read and translate texts",
-            "Participate in conversations",
-        ],
-        answer: { A: "Visual", B: "Auditory", C: "ReadWrite", D: "Kinaesthetic" },
-    },
-    {
-        question: "When assembling furniture, you prefer to:",
-        options: [
-            "Look at diagrams or pictures",
-            "Call a friend for advice",
-            "Read the instructions carefully",
-            "Jump right in and figure it out",
-        ],
-        answer: { A: "Visual", B: "Auditory", C: "ReadWrite", D: "Kinaesthetic" },
-    },
-    {
-        question: "When planning a trip, you find it most helpful to:",
-        options: [
-            "Look at maps and pictures of the destination",
-            "Talk to someone who's been there",
-            "Read guidebooks and reviews",
-            "Explore the destination without a plan",
-        ],
-        answer: { A: "Visual", B: "Auditory", C: "ReadWrite", D: "Kinaesthetic" },
-    },
-    {
-        question: "When trying to understand complex information, you:",
-        options: [
-            "Draw diagrams or flowcharts",
-            "Discuss the information with others",
-            "Write summaries or take detailed notes",
-            "Use physical objects or examples",
-        ],
-        answer: { A: "Visual", B: "Auditory", C: "ReadWrite", D: "Kinaesthetic" },
-    },
-    {
-        question: "When attending a lecture, you find it helpful to:",
-        options: [
-            "Watch the speaker's body language and facial expressions",
-            "Focus on the speaker's voice and tone",
-            "Take detailed notes",
-            "Participate in hands-on activities during the lecture",
-        ],
-        answer: { A: "Visual", B: "Auditory", C: "ReadWrite", D: "Kinaesthetic" },
-    },
-    {
-        question: "When solving a math problem, you prefer to:",
-        options: [
-            "Visualize the problem in your mind",
-            "Talk through the problem out loud",
-            "Write down each step of the solution",
-            "Manipulate objects to represent the problem",
-        ],
-        answer: { A: "Visual", B: "Auditory", C: "ReadWrite", D: "Kinaesthetic" },
-    },
-    {
-        question: "When trying to remember a list of items, you:",
-        options: [
-            "Create mental images for each item",
-            "Repeat the list out loud or listen to someone else say it",
-            "Write the list down",
-            "Physically handle or imagine interacting with each item",
-        ],
-        answer: { A: "Visual", B: "Auditory", C: "ReadWrite", D: "Kinaesthetic" },
-    },
-    {
-        question: "When learning a new sport, you prefer to:",
-        options: [
-            "Watch others play or study diagrams of the field",
-            "Listen to the coach's instructions",
-            "Read about the rules and strategies",
-            "Jump in and start playing",
-        ],
-        answer: { A: "Visual", B: "Auditory", C: "ReadWrite", D: "Kinaesthetic" },
-    },
-    {
-        question: "When preparing a meal, you:",
-        options: [
-            "Look at pictures of the finished dish",
-            "Ask someone for verbal instructions",
-            "Follow a written recipe",
-            "Experiment with ingredients and techniques",
-        ],
-        answer: { A: "Visual", B: "Auditory", C: "ReadWrite", D: "Kinaesthetic" },
-    },
-    {
-        question: "When selecting a new outfit, you:",
-        options: [
-            "Imagine how the clothes will look together",
-            "Ask a friend for their opinion",
-            "Read fashion blogs or descriptions of the items",
-            "Try on different combinations",
-        ],
-        answer: { A: "Visual", B: "Auditory", C: "ReadWrite", D: "Kinaesthetic" },
-    },
-    {
-        question: "When recalling a past event, you tend to:",
-        options: [
-            "Visualize the scene in detail",
-            "Remember the sounds and conversations",
-            "Recount the facts and details",
-            "Focus on how you physically felt or what you were doing",
-        ],
-        answer: { A: "Visual", B: "Auditory", C: "ReadWrite", D: "Kinaesthetic" },
-    },
-];
-
-
 // DOM elements
 const quiz = document.getElementById("quiz");
 const questionText = document.getElementById("question-text");
 const optionContainer = document.getElementById("option-container");
-const submitButton = document.getElementById("submit");
 const result = document.getElementById("result");
 const viewDetails = document.getElementById("view-details");
 const resultScreen = document.getElementById("result-screen");
@@ -187,71 +32,163 @@ window.addEventListener("DOMContentLoaded", () => {
 
 // Load the given question
 function loadQuestion(questionIndex) {
-    questionText.innerText = questions[questionIndex].question;
+    questionText.innerText = questions[questionIndex].text;
+
+    // Clear the previous options
     optionContainer.innerHTML = "";
-    for (const [key, value] of Object.entries(questions[questionIndex].options)) {
-        optionContainer.innerHTML += `
-            <div class="option">
-                <input type="radio" name="option" id="${key}" value="${key}">
-                <label for="${key}">${value}</label>
-            </div>
+
+    // Create the options for the current question
+    questions[questionIndex].options.forEach((option, index) => {
+        const optionElement = document.createElement("div");
+        optionElement.className = "option";
+        optionElement.innerHTML = `
+            <input type="radio" name="option" id="option${index}" value="${index}">
+            <label for="option${index}">${option.text}</label>
         `;
-    }
+
+        optionElement.addEventListener("click", handleOptionClick);
+
+        optionContainer.appendChild(optionElement);
+    });
 }
 
-// Get the selected option
-function getSelectedOption() {
-    const options = document.getElementsByName("option");
-    for (const option of options) {
-        if (option.checked) {
-            return option.value;
-        }
-    }
-    return null;
-}
+// Handle option click
+function handleOptionClick(event) {
+    const optionElement = event.currentTarget;
+    const selectedIndex = parseInt(optionElement.querySelector("input").value);
 
-// Submit the answer and move to the next question or show the result
-submitButton.addEventListener("click", () => {
-    const answer = getSelectedOption();
-    if (!answer) {
-        alert("Please select an answer");
-        return;
-    }
-
-    const selectedAnswer = questions[currentQuestion].answer[answer];
-    score[selectedAnswer]++;
+    const selectedStyle = questions[currentQuestion].options[selectedIndex].style;
+    score[selectedStyle]++;
 
     if (currentQuestion < questions.length - 1) {
         currentQuestion++;
         loadQuestion(currentQuestion);
     } else {
-        quiz.style.display = "none";
-        result.style.display = "flex";
+        finishQuiz();
     }
-});
-
-// Show detailed results on a new screen
-viewDetails.addEventListener("click", () => {
-    showDetailedResults();
-});
-
-function showDetailedResults() {
-    const maxScore = Math.max(...Object.values(score));
-    const userStyle = Object.entries(score).reduce((a, b) => (a[1] > b[1] ? a : b))[0];
-    
-    const totalQuestions = questions.length;
-    const visualPercentage = ((score.Visual / totalQuestions) * 100).toFixed(2);
-    const auditoryPercentage = ((score.Auditory / totalQuestions) * 100).toFixed(2);
-    const readWritePercentage = ((score.ReadWrite / totalQuestions) * 100).toFixed(2);
-    const kinaestheticPercentage = ((score.Kinaesthetic / totalQuestions) * 100).toFixed(2);
-
-    let resultDetails = `You have a predominant ${userStyle} learning style.<br><br>`;
-    resultDetails += `Visual: ${visualPercentage}%<br>`;
-    resultDetails += `Auditory: ${auditoryPercentage}%<br>`;
-    resultDetails += `Read/Write: ${readWritePercentage}%<br>`;
-    resultDetails += `Kinaesthetic: ${kinaestheticPercentage}%<br>`;
-
-    result.style.display = "none";
-    resultScreen.style.display = "flex";
-    resultText.innerHTML = resultDetails;
 }
+
+// Finish the quiz and show the result
+function finishQuiz() {
+    quiz.style.display = "none";
+    result.style.display = "block";
+}
+
+// Calculate the result and show the details
+viewDetails.addEventListener("click", () => {
+    result.style.display = "none";
+    resultScreen.style.display = "block";
+
+    const totalQuestions = questions.length;
+    const percentageScore = {
+        Visual: (score.Visual / totalQuestions) * 100,
+        Auditory: (score.Auditory / totalQuestions) * 100,
+        ReadWrite: (score.ReadWrite / totalQuestions) * 100,
+        Kinaesthetic: (score.Kinaesthetic / totalQuestions) * 100
+    };
+
+    const predominantStyle = Object.keys(score).reduce((a, b) => (score[a] > score[b] ? a : b));
+    const resultMessage = `You are a predominant ${predominantStyle} learner.\n\n`;
+
+    const percentageMessage = Object.entries(percentageScore).map(([style, percentage]) => {
+        return `${style}: ${percentage.toFixed(2)}%`;
+    }).join("\n");
+
+    resultText.innerText = resultMessage + percentageMessage;
+});
+
+// Questions and options
+const questions = [
+    {
+        text: "When you study for a test, do you:",
+        options: [
+            { text: "Read notes and textbooks", style: "ReadWrite" },
+            { text: "Listen to audio recordings", style: "Auditory" },
+            { text: "Use charts, diagrams, and illustrations", style: "Visual" },
+            { text: "Practice physical tasks", style: "Kinaesthetic" }
+        ]
+    },
+    {
+        text: "When learning a new skill, you prefer to:",
+        options: [
+            { text: "Watch a demonstration", style: "Visual" },
+            { text: "Listen to an explanation", style: "Auditory" },
+            { text: "Read step-by-step instructions", style: "ReadWrite" },
+            { text: "Get hands-on experience", style: "Kinaesthetic" }
+        ]
+    },
+    {
+        text: "When attending a lecture, you find it helpful to:",
+        options: [
+            { text: "Take detailed notes", style: "ReadWrite" },
+            { text: "Listen carefully to the speaker", style: "Auditory" },
+            { text: "Visualize concepts in your mind", style: "Visual" },
+            { text: "Fidget or doodle to stay focused", style: "Kinaesthetic" }
+        ]
+    },
+    {
+        text: "Which type of resources do you prefer to use when learning?",
+        options: [
+            { text: "Images, charts, and infographics", style: "Visual" },
+            { text: "Audiobooks and podcasts", style: "Auditory" },
+            { text: "Books and articles", style: "ReadWrite" },
+            { text: "Interactive simulations", style: "Kinaesthetic" }
+        ]
+    },
+    {
+        text: "When trying to remember information, do you:",
+        options: [
+            { text: "Visualize it in your mind", style: "Visual" },
+            { text: "Repeat it out loud or in your head", style: "Auditory" },
+            { text: "Write it down or read it over", style: "ReadWrite" },
+            { text: "Use a physical mnemonic device", style: "Kinaesthetic" }
+        ]
+    },
+    {
+        text: "When solving a complex problem, do you:",
+        options: [
+            { text: "Draw diagrams or mind maps", style: "Visual" },
+            { text: "Talk it through with someone", style: "Auditory" },
+            { text: "Write down a step-by-step plan", style: "ReadWrite" },
+            { text: "Use objects to help visualize the problem", style: "Kinaesthetic" }
+        ]
+    },
+    {
+        text: "When giving directions to someone, do you:",
+        options: [
+            { text: "Draw a map or use hand gestures", style: "Visual" },
+            { text: "Describe the route using landmarks", style: "Auditory" },
+            { text: "Write out detailed instructions", style: "ReadWrite" },
+            { text: "Walk with them to show the way", style: "Kinaesthetic" }
+        ]
+    },
+    {
+        text: "How do you prefer to receive feedback?",
+        options: [
+            { text: "Seeing a visual representation of your progress", style: "Visual" },
+            { text: "Hearing verbal praise or constructive criticism", style: "Auditory" },
+            { text: "Reading written feedback", style: "ReadWrite" },
+            { text: "Receiving hands-on guidance", style: "Kinaesthetic" }
+        ]
+    },
+    {
+        text: "When recalling a past event, what stands out the most?",
+        options: [
+            { text: "The images or visual details", style: "Visual" },
+            { text: "The sounds or conversations", style: "Auditory" },
+            { text: "The words you read or wrote", style: "ReadWrite" },
+            { text: "The physical sensations or emotions", style: "Kinaesthetic" }
+        ]
+    },
+    {
+        text: "Which of these activities do you find the most enjoyable?",
+        options: [
+            { text: "Painting or photography", style: "Visual" },
+            { text: "Playing or listening to music", style: "Auditory" },
+            { text: "Reading or writing", style: "ReadWrite" },
+            { text: "Sports or dancing", style: "Kinaesthetic" }
+        ]
+    }
+    
+    // Add more questions as needed
+];
